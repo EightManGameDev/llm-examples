2779c46ae761f3e464670ea3dae3aebf2dd7d6a2
EightManGameDev <99240068+EightManGameDev@users.noreply.github.com>
2025-02-12 15:34:06 +0000

    1

--- Chatbot.py
+++ Chatbot.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 
 # Configure the page
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by n8n")
+st.title("Nove")
+st.caption("Assisting you in building an empire 🚀")
 
 # N8N production webhook URL
 N8N_WEBHOOK_URL = "https://emperorjosh.app.n8n.cloud/webhook/d7374fd4-5d48-4229-ae39-2ebbfdc9a33f"
