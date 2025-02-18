dfef0126185b21f291838e007e52e331c2326afb
EightManGameDev <99240068+EightManGameDev@users.noreply.github.com>
2025-02-18 21:13:26 +0000

    new webhook

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ st.caption("Assisting you in building an empire 🚀")
 
 # Webhook URLs
 HISTORY_WEBHOOK = "https://emperorjosh.app.n8n.cloud/webhook/3764813c-37c3-412c-b051-377c72a9049a"
-SEND_MESSAGE_WEBHOOK = "https://emperorjosh.app.n8n.cloud/webhook/d7374fd4-5d48-4229-ae39-2ebbfdc9a33f"
+SEND_MESSAGE_WEBHOOK = "https://emperorjosh.app.n8n.cloud/webhook/7337a77e-1ec8-45da-86b9-c06628865d86"
 
 # User & Nova avatars
 USER_AVATAR = "assets/josh.png"
